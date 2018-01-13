5c3323baca6142cb78cc2d20ce791321e748f6e3
Carlos Villanueva <ceva_19@hotmail.com>
2018-01-13 15:25:30 -0300

    Pruebas Unitarias bien estructuradas

--- Testing_with_Junit/ch02-TimeLine/src/test/java/com/twju/ch02/Listing_1_PhasesComments_TimelineTest.java
+++ Testing_with_Junit/ch02-TimeLine/src/test/java/com/twju/ch02/Listing_1_PhasesComments_TimelineTest.java
@@ -5,6 +5,9 @@
  */
 package com.twju.ch02;
 
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
 /**
  *
  * @author barcvilla
@@ -13,5 +16,17 @@ public class Listing_1_PhasesComments_TimelineTest
 {
     private final static int NEW_FETCH_COUNT = TimeLine.DEFAULT_FETCH_COUNT + 1;
     
+    @Test
+    public void setFetchCount()
+    {
+        // 1. configuracion (organizar y compilar)
+        TimeLine timeLine = new TimeLine();
+        
+        // 2. ejercicio (actuar, operar)
+        timeLine.setFetchCount(NEW_FETCH_COUNT);
+        
+        // 3. verificar (afirmar, verificar)
+        assertEquals(NEW_FETCH_COUNT, timeLine.getFetchCount());
+    }
     
 }
